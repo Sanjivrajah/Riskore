72cd7d24506add324eec8d45635012d79c704bf1
Hans Andreanto <andreantohanss@gmail.com>
2024-11-30 11:28:23 +0800

    added new-models

--- lib/core/models/user_data.dart
+++ lib/core/models/user_data.dart
@@ -4,12 +4,20 @@ class UserData {
   DateTime birthdate; 
   String place; 
   List<String> connectedEWallets; 
+  String employment_status;
+  String occupations;
+  double monthly_income;
+  List<String> hobbies; 
 
   UserData({
     required this.userId,
     required this.birthdate,
     required this.place,
     required this.connectedEWallets,
+    required this.employment_status, 
+    required this.occupations,
+    required this.monthly_income, 
+    required this.hobbies
   });
 
   // Method to convert UserData to a Map for Firebase
@@ -19,6 +27,10 @@ class UserData {
       'birthdate': birthdate.toIso8601String(),
       'place': place,
       'connectedEWallets': connectedEWallets,
+      'employment_status':employment_status,
+      'occupations': occupations,
+      'monthly_income': monthly_income,
+      'hobbies': hobbies
     };
   }
 
@@ -29,6 +41,10 @@ class UserData {
       birthdate: DateTime.parse(map['birthdate']),
       place: map['place'],
       connectedEWallets: List<String>.from(map['connectedEWallets']),
+      employment_status: map['employment_status'],
+      occupations: map['occupations'],
+      monthly_income: map['monthly_income'],
+      hobbies: List<String>.from(map['hobbies']),
     );
   }
 }
